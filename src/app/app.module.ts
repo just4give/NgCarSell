81cb5de223c3716d4a4c1c2f99931e88501a0aa2
mithun.das <mithun.das@dealertrack.com>
2018-01-23 16:55:57 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BuyComponent } from './buy/buy.component';
+import {SellComponent} from "./sell/sell.component";
 
+const appRoutes: Routes = [
+  { path: 'buy', component: BuyComponent },
+  { path: 'sell',      component: SellComponent },
+  { path: '**', component: SellComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SellComponent,
+    BuyComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
